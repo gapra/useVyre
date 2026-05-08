6d507d5faa6623f028451ca65f220851687f782b
gapra <gapraart@gmail.com>
2026-05-08 12:43:50 +0700

    docs: udpate packages urls

---
+++ packages/eslint-plugin/src/index.js
@@ -0,0 +1,33 @@
+import { noInvalidVyreProps } from "./rules/no-invalid-vyre-props.js";
+
+const plugin = {
+  meta: {
+    name: "@usevyre/eslint-plugin",
+    version: "0.1.0",
+  },
+  rules: {
+    "no-invalid-vyre-props": noInvalidVyreProps,
+  },
+  configs: {},
+};
+
+// Flat config (ESLint 9+)
+plugin.configs.recommended = {
+  name: "vyre/recommended",
+  plugins: { vyre: plugin },
+  rules: {
+    "vyre/no-invalid-vyre-props": "error",
+  },
+};
+
+// Legacy config (ESLint 8)
+plugin.configs["recommended-legacy"] = {
+  plugins: ["vyre"],
+  rules: {
+    "vyre/no-invalid-vyre-props": "error",
+  },
+};
+
+export default plugin;
+export const rules = plugin.rules;
+export const configs = plugin.configs;
